43003efa3ecbcabe825400e524a01ea3af8275b2
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-14 12:27:39 +0530

    Exam Portal Application

--- ExamServer/src/main/java/com/exam/ExamServerApplication.java
+++ ExamServer/src/main/java/com/exam/ExamServerApplication.java
@@ -1,13 +1,18 @@
 package com.exam;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamServerApplication {
+public class ExamServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamServerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Starting code");
+	}
 }
---
+++ ExamServer/src/main/java/com/exam/service/impl/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.exam.service.impl;
+
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.repository.RoleRepository;
+import com.exam.repository.UserRepository;
+import com.exam.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    //    creating user
+    @Override
+    public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+
+        User local = this.userRepository.findByUsername(user.getUsername());
+        if(local != null){
+            System.out.println("User already present!!");
+            throw new Exception("User exits !!");
+        }
+        else {
+//            user create
+            for(UserRole ur : userRoles)
+            {
+                roleRepository.save(ur.getRole());
+            }
+            user.getUserRoles().addAll(userRoles);
+            local = this.userRepository.save(user);
+        }
+        return local;
+    }
+}
