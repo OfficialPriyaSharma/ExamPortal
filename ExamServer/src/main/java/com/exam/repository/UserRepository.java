571ea08f8dc239aff22aadf70b86e18c68f96724
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-16 12:16:10 +0530

    Exam Portal Application

--- ExamServer/src/main/java/com/exam/repository/UserRepository.java
+++ ExamServer/src/main/java/com/exam/repository/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    public User findByUserName(String username);
+    public User findByUsername(String username);
 }
--- ExamServer/src/main/java/com/exam/service/UserServiceImpl.java
+++ ExamServer/src/main/java/com/exam/service/UserServiceImpl.java
@@ -23,7 +23,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user, Set<UserRole> userRoles) throws Exception {
 
-        User local = this.userRepository.findByUserName(user.getUsername());
+        User local = this.userRepository.findByUsername(user.getUsername());
         if(local != null){
             System.out.println("User already present !!");
             throw new Exception("User exits !!");
