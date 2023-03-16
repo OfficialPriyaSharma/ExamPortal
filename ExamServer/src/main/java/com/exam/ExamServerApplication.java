76699edddf4f264e233bfe91eac0a328ca18f843
Priya Sharma <101420743+OfficialPriyaSharma@users.noreply.github.com>
2023-03-16 12:56:25 +0530

    Exam Portal Application

--- ExamServer/src/main/java/com/exam/ExamServerApplication.java
+++ ExamServer/src/main/java/com/exam/ExamServerApplication.java
@@ -27,28 +27,28 @@ public class ExamServerApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("Starting code");
 
-		User user = new User();
-
-		user.setFirstName("Priya");
-		user.setLastName("Sharma");
-		user.setUsername("priya27");
-		user.setPassword("priyaaa");
-		user.setEmail("priyaaa@gmail.com");
-		user.setProfile("default.png");
-
-		Role role1 = new Role();
-		role1.setRoleId(11L);
-		role1.setRoleName("ADMIN");
-
-		Set<UserRole> userRoleSet = new HashSet<>();
-		UserRole userRole = new UserRole();
-		userRole.setRole(role1);
-		userRole.setUser(user);
-
-		userRoleSet.add(userRole);
-
-		User user1 = this.userService.createUser(user,userRoleSet);
-		System.out.println(user1.getUsername());
-
+//		User user = new User();
+//
+//		user.setFirstName("Priya");
+//		user.setLastName("Sharma");
+//		user.setUsername("priya27");
+//		user.setPassword("priyaaa");
+//		user.setEmail("priyaaa@gmail.com");
+//		user.setProfile("default.png");
+//
+//		Role role1 = new Role();
+//		role1.setRoleId(11L);
+//		role1.setRoleName("ADMIN");
+//
+//		Set<UserRole> userRoleSet = new HashSet<>();
+//		UserRole userRole = new UserRole();
+//		userRole.setRole(role1);
+//		userRole.setUser(user);
+//
+//		userRoleSet.add(userRole);
+//
+//		User user1 = this.userService.createUser(user,userRoleSet);
+//		System.out.println(user1.getUsername());
+//
 	}
 }
